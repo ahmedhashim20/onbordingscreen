c0e190397193ab6d01fd8e9999c6fa75ca780251
ahmedhashim20 <hashimindhoye@gmail.com>
2025-05-02 03:35:52 -0700

    Add full Flutter project files

---
+++ android/app/src/main/kotlin/com/example/onbordingscreen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.onbordingscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Onboboarding/onboarding_items.dart
@@ -0,0 +1,29 @@
+import 'onboarding_info.dart';
+
+class OnboardingItems {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+      title: "Fuundi",
+      descriptions: "Fuundi waa xirfadle dhisaya albaabada, daaqadaha, iyo alaabta guriga si qurux badan oo adkeysi leh.",
+      image: "assets/sawir1.jpg",
+    ),
+
+    OnboardingInfo(
+      title: "Rinjile",
+      descriptions: "Rinjilaha wuxuu qurxiyaa derbiyada iyo saqafyada, isagoo u roonaya guryaha midab iyo nadiifnimo cusub.",
+      image: "assets/sawir2.jpg",
+    ),
+
+    OnboardingInfo(
+      title: "Tubiste",
+      descriptions: "Tubo yaqaan wuxuu hagaajiyaa biyaha iyo dhuumaha, isagoo hubinaya in guryahaagu leeyihiin biyo nadiif ah oo socda.",
+      image: "assets/sawir3.jpg",
+    ),
+
+    OnboardingInfo(
+      title: "Korantiste",
+      descriptions: "Korantistaha wuxuu xallinayaa ciladaha korontada, wuxuuna hubiyaa in guryahaaga iyo xafiisyadaagu si nabad ah u shaqeeyaan.",
+      image: "assets/sawir4.jpg",
+    ),
+  ];
+}
